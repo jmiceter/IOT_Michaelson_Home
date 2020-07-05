da986564cedf8ca71f83bc1d1ac6903f89826be8
jmiceter <jonathan_michaelson@yahoo.com>
2020-07-05 15:31:57 -0700

    ccs dependancies

---
+++ Debug/Main.d
@@ -0,0 +1,15 @@
+# FIXED
+
+Main.obj: ../Main.c
+Main.obj: C:/ti/ccsv8/ccs_base/msp430/include/msp430.h
+Main.obj: C:/ti/ccsv8/ccs_base/msp430/include/msp430g2533.h
+Main.obj: C:/ti/ccsv8/ccs_base/msp430/include/in430.h
+Main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics.h
+Main.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics_legacy_undefs.h
+
+../Main.c: 
+C:/ti/ccsv8/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv8/ccs_base/msp430/include/msp430g2533.h: 
+C:/ti/ccsv8/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.1.LTS/include/intrinsics_legacy_undefs.h: 
